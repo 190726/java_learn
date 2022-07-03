ab7929516ff5926685408039e25cf7f24259de9c
DESKTOP-RCJOS5S\sangkook <sixstar6@naver.com>
2022-07-03 12:41:59 +0900

    java stream study

--- develop-tech/src/main/java/com/sk/Main.java
+++
@@ -1,7 +0,0 @@
-package com.sk;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ technique/src/main/java/com/sk/laul/ch02/MainApplication.java
@@ -0,0 +1,8 @@
+package com.sk.laul.ch02;
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+        System.out.println("----");
+    }
+}
